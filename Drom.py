8537a956c28ae08f9d07459cfbd978d230ee5990
mikerockin <45920169+mikerockin@users.noreply.github.com>
2021-12-17 09:48:39 +0500

    Update Drom.py

--- Drom.py
+++ Drom.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 dr = webdriver.Chrome()
 dr.get('https://my.drom.ru/sign?return=https%3A%2F%2Fwww.drom.ru%2F%3Ftcb%3D1639714709')
 username = dr.find_element_by_xpath('//*[@id="sign"]')
-username.send_keys('mrmikhail88@gmail.com')
+username.send_keys('') #Ввести логин в ''
 password = dr.find_element_by_xpath('//*[@id="password"]')
-password.send_keys('6as6r3e8')
+password.send_keys('') #Ввести пароль в ''
 login = dr.find_element_by_xpath('//*[@id="signbutton"]')
 login.click()
