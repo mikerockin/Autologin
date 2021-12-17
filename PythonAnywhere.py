aab28e65846300b3be9d41f141cdd0b00dda8442
mikerockin <45920169+mikerockin@users.noreply.github.com>
2021-12-17 09:49:55 +0500

    Update PythonAnywhere.py

--- PythonAnywhere.py
+++ PythonAnywhere.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 pt = webdriver.Chrome()
 pt.get('https://www.pythonanywhere.com/login/')
 username = pt.find_element_by_xpath('//*[@id="id_auth-username"]')
-username.send_keys('MikeRockIn')
+username.send_keys('')#Ввести логин в ''
 password = pt.find_element_by_xpath('//*[@id="id_auth-password"]')
-password.send_keys('yXZbRu2aFe')
+password.send_keys('')#Ввести пароль в ''
 login = pt.find_element_by_xpath('//*[@id="id_next"]')
 login.click()
