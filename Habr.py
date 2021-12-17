d611ab51bb0dc501cfceb6b00b2962b607392381
mikerockin <45920169+mikerockin@users.noreply.github.com>
2021-12-17 09:49:28 +0500

    Update Habr.py

--- Habr.py
+++ Habr.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 hb =  webdriver.Chrome()
 hb.get('https://account.habr.com/login/')
 username = hb.find_element_by_xpath('//*[@id="email_field"]')
-username.send_keys('mrmikhail88@gmail.com')
+username.send_keys('') #Ввести логин в ''
 password = hb.find_element_by_xpath('//*[@id="password_field"]')
-password.send_keys('yXZbRu2aFe')
+password.send_keys('') #Ввести пароль в ''
 login = hb.find_element_by_xpath('//*[@id="login_form"]/fieldset/div[4]/button')
 login.click()
