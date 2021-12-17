0af1de23ee5a2d0b7d3ce22a2a33705b45977a2a
mikerockin <45920169+mikerockin@users.noreply.github.com>
2021-12-17 09:48:29 +0500

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 fb = webdriver.Chrome()
 fb.get('https://www.facebook.com/login')
 username = fb.find_element_by_xpath('//*[@id="email"]')
-username.send_keys('mrmikhail88@gmail.com')
+username.send_keys('') #Ввести логин в ''
 password = fb.find_element_by_xpath('//*[@id="pass"]')
-password.send_keys('yXZbRu2aFe')
+password.send_keys('') #Ввести пароль в ''
 login = fb.find_element_by_xpath('//*[@id="loginbutton"]')
 login.click()
