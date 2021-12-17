9de9a45a302b015c808e80227cc03966ab188802
mikerockin <45920169+mikerockin@users.noreply.github.com>
2021-12-17 09:50:23 +0500

    Update VKcom.py

--- VKcom.py
+++ VKcom.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 vk = webdriver.Chrome()
 vk.get('https://vk.com/')
 username = vk.find_element_by_xpath('//*[@id="index_email"]')
-username.send_keys('mrmikhail88@gmail.com')
+username.send_keys('')#Ввести логин в ''
 password = vk.find_element_by_xpath('//*[@id="index_pass"]')
-password.send_keys('yXZbRu2aFeee')
+password.send_keys('') #Ввести пароль в ''
 login = vk.find_element_by_xpath('//*[@id="index_login_button"]')
 login.click()
